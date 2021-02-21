bf6da25fe64b96384d1d3e4a46f75b542b3bfc33
Darshan N Kini <78312855+kinidarshan@users.noreply.github.com>
2021-02-21 11:35:32 +0530

    Case Study

---
+++ TrainDAO.java
@@ -0,0 +1,17 @@
+package ticketapp;
+
+
+public class TrainDAO {
+	String DRIVER_NAME;
+	String DB_URL;
+	String USERNAME;
+	String PASSWORD;
+
+	public Train findTrain(int trainNo)
+	{
+		return null;
+
+	}
+
+
+}
